67348a63ff7fed3e81743d96dfd66b4a2ddbf5ab
Avery Wardrup <averydwardrup@users.noreply.github.com>
2024-02-04 23:15:58 -0500

    Create theme.js

---
+++ vuetify/theme.js
@@ -0,0 +1,12 @@
+const draculaTheme = {
+    dark: true,
+    colors: {
+        primary: '#BD93F9', // Dracula Purple
+        secondary: '#6272A4', // Dracula Blue
+        accent: '#FF79C6', // Dracula Pink
+        error: '#FF5555', // Dracula Red
+        info: '#8BE9FD', // Dracula Cyan
+        success: '#50FA7B', // Dracula Green
+        warning: '#FFB86C', // Dracula Orange
+    }
+}
